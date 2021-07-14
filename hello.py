c02b6ec77a0a08d42e0ee721972c9e62d95be55e
que-ai <1451881137@qq.com>
2021-07-14 23:30:46 +0800

    3.5静态文件

--- hello.py
+++ hello.py
@@ -1,11 +1,40 @@
-from flask import Flask
+"""
+测试主页
+"""
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """
+    404错误处理
+    """
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    """
+    500错误处理
+    """
+    return render_template('500.html'), 500
 
 
 @app.route('/')
 def index():
-	return 'hello world'
+    """
+    首页
+    """
+    return render_template('index.html')
 
 
-
+@app.route('/user/<name>')
+def user(name):
+    """
+    变量测试
+    """
+    return render_template('user.html', name=name)
