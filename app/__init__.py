b877a93d33ea911b8faf11ea27507e78e50aa2cc
que-ai <1451881137@qq.com>
2021-07-16 01:02:09 +0800

    login and logout

--- app/__init__.py
+++ app/__init__.py
@@ -8,12 +8,14 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from config import config
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 monet = Moment()
 db = SQLAlchemy()
 mail = Mail()
-
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -24,8 +26,12 @@ def create_app(config_name):
     monet.init_app(app)
     db.init_app(app)
     mail.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/auth')
 
     return app
--- flasky.py
+++ flasky.py
@@ -1,18 +1,22 @@
 """
 入口文件
 """
-from app import create_app,db
+from app import create_app, db
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app.models import User,Role
+from app.models import User, Role
 import click
 
-app=create_app('development')
+app = create_app('development')
 
 manager = Manager(app)
 Migrate(app, db)
 manager.add_command("db", MigrateCommand)
 
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
 
 
 if __name__ == '__main__':
